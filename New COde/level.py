1716d2044ee98aff38f105d94e738b97f9fda66f
MKuffskie <mkuffskie1@gmail.com>
2024-11-20 16:32:28 -0500

    Update level.py

--- New COde/level.py
+++ New COde/level.py
@@ -43,7 +43,11 @@ class Level:
 
 		# trees 
 		for obj in tmx_data.get_layer_by_name('Trees'):
-			Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.tree_sprites], obj.name)
+			Tree(	pos = (obj.x, obj.y), 
+				surf = obj.image, 
+				groups = [self.all_sprites, self.collision_sprites, self.tree_sprites], 
+				name = obj.name,
+				player_add = self.player_add)
 
 		# wildflowers 
 		for obj in tmx_data.get_layer_by_name('Decoration'):
