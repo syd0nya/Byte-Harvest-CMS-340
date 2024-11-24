9eada1d4288986493f5534123fe591132fa3a071
MKuffskie <mkuffskie1@gmail.com>
2024-11-24 12:57:47 -0500

    Updated look and some functionality

--- ByteHarvest_withTextEditor/code/editor.py
+++ ByteHarvest_withTextEditor/code/editor.py
@@ -13,14 +13,14 @@ class Editor:
 
         # Initialize pygame's built-in font module to make a font object
         pygame.font.init()
-        self.font = pygame.font.SysFont('arial.ttf', 32) # Default pygame font
+        self.font = pygame.font.SysFont("couriernew", 24) # Monospaced pygame font
 
         # Render initial text
-        self.text = self.font.render('', True, 'black', 'white') # Gray text on white background
+        self.text = self.font.render('', True, 'black', 'white') # Black text on white background
 
         # Set the text to appear in the center of the page
         self.textRect = self.text.get_rect()
-        self.textRect.center = (SCREEN_WIDTH/2 - 360, SCREEN_HEIGHT/5)
+        self.textRect.center = (SCREEN_WIDTH/2 - 450, SCREEN_HEIGHT/5)
 
         self.pageOffset = 0
 
@@ -28,12 +28,22 @@ class Editor:
         self.userInput = ""
 
     def run(self):        
+        # Make the background color with current day color
+        self.display_surface.fill((114, 183, 219)) # light blue
 
         # Make a white rectangle for the actual page
         pygame.draw.rect(self.display_surface, 'white', pygame.Rect((SCREEN_WIDTH/2)-(PAGE_WIDTH/2), 
-                                                                    (SCREEN_HEIGHT/8) + self.pageOffset, 
+                                                                    (SCREEN_HEIGHT/8), 
                                                                     PAGE_WIDTH, PAGE_HEIGHT))
         
+        # Draw the margins on the page
+        pygame.draw.rect(self.display_surface, 'black', pygame.Rect((SCREEN_WIDTH/2) - 460,
+                                                                    (SCREEN_HEIGHT/8),
+                                                                    2, PAGE_HEIGHT))
+        pygame.draw.rect(self.display_surface, 'black', pygame.Rect((SCREEN_WIDTH/2) + 460,
+                                                                    (SCREEN_HEIGHT/8),
+                                                                    2, PAGE_HEIGHT))
+
         event_list = pygame.event.get()
         self.handle_events(event_list)
 
@@ -47,6 +57,8 @@ class Editor:
         self.display_surface.blit(self.text, self.textRect)
 
 
+    # Handle all the key strokes
+    # (If you try to do it in the run method pygame misses some of the keystrokes)
     def handle_events(self, events):
         # If any event happens, check for user input
         shift = False
@@ -71,6 +83,10 @@ class Editor:
                 # Is it a space
                 elif event.key == 32:
                     self.userInput += " "
+
+                # Is it enter
+                elif event.key == 13:
+                    self.userInput = self.userInput.ljust(MAX_CHARACTERS)
                     
 
                 
